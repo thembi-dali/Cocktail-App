639334ae5c68f3a05df3bc736949e31ddfc2ce90
Thembi Baleni <thembi.baleni@gmail.com>
2021-02-25 13:18:10 +1100

    Added routes & Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:show, :index, :new, :create] do
+  resources :doses, only: [:create, :destroy]
+  end
+  resources :ingredients, only: [:show]
+
+  root "cocktails#index"
 end
